65e0ab1696c81c263c4f62f5b7e375917d69f6ed
Harsh.Trivedi <trivediharsh49@gmail.com>
2020-11-01 01:52:55 +0530

    Update Round C 2020 - Kick Start 2020-countdown.cpp

--- Round C 2020 - Kick Start 2020-countdown.cpp
+++ Round C 2020 - Kick Start 2020-countdown.cpp
@@ -1,3 +1,29 @@
+/*Problem
+Avery has an array of N positive integers. The i-th integer of the array is Ai.
+
+A contiguous subarray is an m-countdown if it is of length m and contains the integers m, m-1, m-2, ..., 2, 1 in that order. For example, [3, 2, 1] is a 3-countdown.
+
+Can you help Avery count the number of K-countdowns in her array?
+
+Input
+The first line of the input gives the number of test cases, T. T test cases follow. Each test case begins with a line containing the integers N and K. The second line contains N integers. The i-th integer is Ai.
+
+Output
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the number of K-countdowns in her array.
+
+Limits
+Time limit: 20 seconds per test set.
+Memory limit: 1GB.
+1 ≤ T ≤ 100.
+2 ≤ K ≤ N.
+1 ≤ Ai ≤ 2 × 105, for all i.
+
+Test set 1
+2 ≤ N ≤ 1000.
+
+Test set 2
+2 ≤ N ≤ 2 × 105 for at most 10 test cases.
+For the remaining cases, 2 ≤ N ≤ 1000.*/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -43,4 +69,5 @@ int main()
 		cout<<"Case #"<<i+1<<": "<<z<<"\n";
 		z=0;
 	}
+	return 0;
 }
